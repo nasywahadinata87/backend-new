68c42a23855841098bb99355cf3e20f945142bea
nasywahadinata87 <nasywahadinata87@gmail.com>
2026-07-11 12:27:30 +0700

    Enable CORS

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const db = require("./db");
 
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
